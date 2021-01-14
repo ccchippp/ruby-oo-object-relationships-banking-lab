2981001a253dc4a1056d178a6c467a8941650790
ccchippp <clempke90@gmail.com>
2021-01-14 17:12:03 -0500

    4 problems left

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
+    require 'pry'
 
+    attr_reader :name, :display_balance
+    attr_accessor :balance, :status
+    attr_writer :deposit
+    
+    def initialize(name)
+        @name = name
+        @balance = balance=1000
+        @status = status='open'
+        #binding.pry
+    end
+     def deposit(deposit_amount)
+         @balance += deposit_amount
+     end
+     def display_balance
+        "Your balance is $#{@balance}."
+     end
+     def valid?
+        if @balance > 0 && @status == "open"
+            return true
+        else
+            return false
+        end
+    end       
+    def close_account
+        @balance = 0
+        @status = 'closed'
+    end
+  
 end
+bezos = BankAccount.new("Bezos")
+# binding.pry
+puts bezos.display_balance
+bezos.deposit(1000)
+puts bezos.display_balance
+puts bezos.valid?
+bill = BankAccount.new("Bill")
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+
+require_relative './bank_account.rb'
+require 'pry'
+
+
+
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  attr_reader :hi
+
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status = 'pending'
+    @amount = amount 
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+   #binding.pry
+
+  def execute_transaction
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = 'complete'
+  end
+ 
 end
+
+ 
+#test.transfer_money
+#binding.pry
+# puts bezos.display_balance
+# test_transfer.execute_transfer
+# puts bezos.balance
+# puts bezos.close_account
+
